9987373f05ee204372f9a17efb1570b939c15d8e
march101348 <12351235ti@gmail.com>
2022-05-04 21:16:58 +0900

    #8 eslint, prettier, huskyの整備

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const App = () => <div className="App" />;
 
 export default App;
