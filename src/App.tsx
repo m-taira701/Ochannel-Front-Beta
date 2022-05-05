100c1eb9aee707804c58fec6f512d4de07c66b24
march101348 <12351235ti@gmail.com>
2022-05-05 18:03:47 +0900

    #9 formatter適用

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home } from './components/pages/Home';
 
-const App = () => <div className="App">
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<Home />} />
-    </Routes>
-  </BrowserRouter>
-</div>;
+const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,11 +1,11 @@
-import { Container, Box, Typography } from "@mui/material"
+import { Container, Box, Typography } from '@mui/material';
 
 export const Home: React.FC = () => (
-    <Container maxWidth="sm">
+  <Container maxWidth="sm">
     <Box sx={{ my: 4 }}>
       <Typography variant="h4" component="h1" gutterBottom>
         Create React App example
       </Typography>
     </Box>
   </Container>
-)
+);
